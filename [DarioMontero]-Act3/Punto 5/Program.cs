bb602cc4c772e54d2d54f1a5e8d32b5b919b9981
DarioBenjaminMontero <dariomontero2008@gmail.com>
2026-03-25 22:01:38 -0300

    actividad 3

---
+++ [DarioMontero]-Act3/Punto 5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            string linea;
+            Console.Write("escribe una coordenada x: ");
+            linea = Console.ReadLine();
+            x = int.Parse(linea);
+            Console.Write("escribe una coordenada y: ");
+            linea = Console.ReadLine();
+            y = int.Parse(linea);
+
+            if (x != 0 && y != 0) {
+
+                if (x > 0 && y > 0) {
+                    Console.Write("Cuadrante 1");
+                }
+                if (x < 0 && y > 0) {
+                    Console.Write("Cuadrante 2");
+                }
+                if (x < 0 && y < 0) {
+                    Console.Write("Cuadrante 3");
+                }
+                if (x > 0 && y < 0)
+                {
+                    Console.Write("Cuadrante 4");
+                }
+
+
+            }
+        }
+    }
+}
