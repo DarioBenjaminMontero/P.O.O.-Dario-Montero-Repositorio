3089307323207cd1de461456132eceb5c82942c9
DarioBenjaminMontero <dariomontero2008@gmail.com>
2026-04-10 11:59:56 -0300

    punto 1 hecho, comienzo del punto 2

---
+++ [DarioMontero]-Act8/punto 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto_1
+{
+    internal class Elementos
+    {
+        private int[] vector;
+        private int min;
+        public void Cargar() {
+            string linea;
+            Console.WriteLine("cuantos elementos vas a colocar en este vector?");
+            linea = Console.ReadLine();
+            string linea2;
+            int numero = int.Parse(linea);
+            vector = new int[numero];
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine("coloca el numero en la posicion " + i);
+                linea2 = Console.ReadLine();
+                vector[i] = int.Parse(linea2);
+                if (i == 0)
+                {
+                    min = vector[i];
+                    Console.WriteLine("ahora el numero menor es " + vector[i]);
+                }
+                else if (vector[i] < min)
+                {
+                    min = vector[i];
+                    Console.WriteLine("ahora el menor es " + min);
+                }
+                else if (vector[i] == min)
+                {
+                    Console.WriteLine("el numero se repitio");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Elementos elemento = new Elementos();
+            elemento.Cargar();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ [DarioMontero]-Act8/punto 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto_2
+{
+    internal class Program
+    {
+
+        private int[] vector;
+
+        public void cargar() {
+
+            vector = new int[5];
+
+        }
+
+        static void Main(string[] args)
+        {
+
+        
+
+
+        }
+    }
+}
