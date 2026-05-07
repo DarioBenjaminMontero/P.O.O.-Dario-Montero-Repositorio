aacb57addef54320a18ec3ca6c1fb41a71ebfdb1
DarioBenjaminMontero <dariomontero2008@gmail.com>
2026-05-07 08:16:21 -0300

    subo avances de la actividad 11

---
+++ [DarioMontero]-Act11/Punto 1/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto_1
+{
+    internal class temperaturas
+    {
+        /*1. Se desea saber la temperatura media trimestral de cuatro paises. Para ello se tiene como
+dato las temperaturas medias mensuales de dichos paises.
+Se pide ingresar el nombre del país y seguidamente las tres temperaturas medias
+mensuales.
+Seleccionar las estructuras de datos adecuadas para el almacenamiento de los datos en
+memoria.
+a. Cargar por teclado los nombres de los paises y las temperaturas medias mensuales.
+b. Imprimir los nombres de las paises y las temperaturas medias mensuales de las
+mismas.
+c. Calcular la temperatura media trimestral de cada país.
+d. Imprimir los nombres de los paises y las temperaturas medias trimestrales.
+e. Imprimir el nombre del país con la temperatura media trimestral mayor.*/
+
+        private string[] paises;
+        private float[,] matTemperaturas;
+        private float[] tempTrimestral;
+        private float mayorTemp;
+        public void cargar() {
+            string linea;
+            paises = new string[4];
+            matTemperaturas = new float[4, 3];
+            tempTrimestral = new float[4];
+            for (int i = 0; i < paises.Length; i++) {
+                Console.WriteLine("escribe un pais por favor");
+                paises[i] = Console.ReadLine();
+                for (int m = 0; m < matTemperaturas.GetLength(1); m++) {
+                    Console.WriteLine("escribe la temperatura numero " + m + " del pais " + paises[i]);
+                    linea = Console.ReadLine();
+                    matTemperaturas[i,m] = float.Parse(linea);
+                }
+            }
+        }
+        public void mostrarTemperaturas() {
+            for (int i = 0; i < paises.Length; i++) {
+                Console.WriteLine("el pais: " + paises[i] + " tiene las temperaturas:");
+                for (int m = 0; m < matTemperaturas.GetLength(1); m++)
+                {
+                    Console.WriteLine("en el mes "+m + " tuvo una temperatura de: " + matTemperaturas[i,m]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public void calcularMedia() {
+            float media;
+            float suma;
+            for (int i = 0; i < paises.Length; i++)
+            {
+                suma = 0;
+                for (int m = 0; m < matTemperaturas.GetLength(1); m++)
+                {
+                    suma += matTemperaturas[i, m];
+                }
+                media = suma / matTemperaturas.GetLength(1);
+                tempTrimestral[i] = media;
+            }
+        }
+
+        public void imprimirTrimes() {
+            for (int i = 0; i < paises.Length; i++) {
+                Console.WriteLine("el pais " + paises[i] + " tuvo una temperatura trimestral media de: " + tempTrimestral[i] + " grados");
+            }
+        }
+        public void encontrarMayor() {
+            mayorTemp = tempTrimestral[0];
+            for (int i = 0; i < paises.Length; i++) {
+                if (tempTrimestral[i] > mayorTemp) {
+                mayorTemp= tempTrimestral[i];
+                }
+            }
+        }
+        public void imprimirMayor() {
+            for (int i = 0; i < paises.Length; i++) {
+                if (tempTrimestral[i] == mayorTemp) {
+                    Console.WriteLine("el pais " + paises[i] + " tuvo la mayor temperatura trimestral con: " + tempTrimestral[i] + " grados");
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            temperaturas temp = new temperaturas();
+            temp.cargar();
+            temp.mostrarTemperaturas();
+            temp.calcularMedia();
+            temp.imprimirTrimes();
+            temp.encontrarMayor();
+            temp.imprimirMayor();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ [DarioMontero]-Act11/punto 2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto_2
+{
+    internal class matrizIrregular
+    {
+        /*2. Confeccionar una clase para administrar una matriz irregular de 5 filas y 1 columna la
+primer fila, 2 columnas la segunda fila y así sucesivamente hasta 5 columnas la última fila
+(crearla sin la intervención del operador)
+Realizar la carga por teclado e imprimir posteriormente.*/
+
+
+        private int[][] irregular;
+
+        public void creacion() {
+            irregular = new int[5][];
+            for (int i = 0; i < irregular.GetLength(0); i++) {
+                irregular[i] = new int[i+1];
+            }
+        }
+        public void cargar() {
+            string linea;
+            for (int i = 0; i < irregular.GetLength(0); i++) {
+                for (int f = 0; f < irregular[i].GetLength(1); i++) {
+                    Console.WriteLine("escribe el valor de la fila " + (i + 1) + " en la columna " + (f+1)); 
+                    linea = Console.ReadLine();
+                    irregular[i][f] = int.Parse(linea);
+                }
+            }
+        }
+        public void imprimir()
+        {
+            for (int i = 0; i < irregular.GetLength(0); i++)
+            {
+                for (int f = 0; f < irregular[i].GetLength(1); i++)
+                {
+                    Console.Write(irregular[i][f] + "  ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            matrizIrregular mat = new matrizIrregular();
+            mat.creacion();
+            mat.cargar();
+            mat.imprimir();
+            Console.ReadKey(); 
+        }
+    }
+}
