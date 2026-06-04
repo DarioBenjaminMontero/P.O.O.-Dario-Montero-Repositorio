1fed6ee6ec20922ba85972332afe4b580c93c2a9
Capone567 <caponethiagomanuel@gmail.com>
2026-06-04 08:59:28 -0300

    termino 2 y 3

---
+++ AdopcionMascotas/AdopcionMascotas/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdopcionMascotas
+{
+    public class Animal
+    {
+        public string Nombre { get; }
+        public string Raza { get; }
+
+        public Animal(string nombre, string raza)
+        {
+            this.Nombre = nombre;
+            this.Raza = raza;
+        }
+
+        public virtual string HacerSonido()
+        {
+            return "sonidoGenerico";
+        }
+    }
+    public class Perro : Animal
+    {
+        public Perro(string nombre, string raza) : base(nombre, raza) { }
+
+        public override string HacerSonido()
+        {
+            return "Wof";
+        }
+    }
+
+    public class Gato : Animal
+    {
+        public Gato(string nombre, string raza) : base(nombre, raza) { }
+
+        public override string HacerSonido()
+        {
+            return "Miau";
+        }
+    }
+
+    public class Persona
+    {
+        public string Nombre { get; }
+        public string Dni { get; }
+
+        public List<Animal> MascotasAdoptadas { get; }
+
+        public Persona(string nombre, string dni)
+        {
+            this.Nombre = nombre;
+            this.Dni = dni;
+            this.MascotasAdoptadas = new List<Animal>();
+        }
+    }
+    internal class Program
+    {
+        static List<Animal> animalesDisponibles = new List<Animal>();
+        static List<Persona> personasRegistradas = new List<Persona>();
+        static void Main(string[] args)
+        {
+            animalesDisponibles.Add(new Perro("Boby", "Bulldog"));
+            animalesDisponibles.Add(new Gato("Paca", "Rex"));
+            animalesDisponibles.Add(new Gato("Fito", "Gato Europeo"));
+
+            personasRegistradas.Add(new Persona("Juan Chiriff", "1234"));
+            personasRegistradas.Add(new Persona("Simon Basante", "5678"));
+
+            string opcion = "";
+            while (opcion != "5")
+            {
+                Console.WriteLine("Menú del centro de adopción.");
+                Console.WriteLine("1. Mostrar animales disponibles");
+                Console.WriteLine("2. Registrar nueva persona");
+                Console.WriteLine("3. Adoptar una mascota");
+                Console.WriteLine("4. Mostrar adoptantes");
+                Console.WriteLine("5. Salir");
+                Console.Write("Seleccione una opción: ");
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Animales disponible: ");
+                        for (int i = 0; i < animalesDisponibles.Count; i++)
+                        {
+                            Animal a = animalesDisponibles[i];
+                            Console.WriteLine($"[{i}] Nombre: {a.Nombre} | Raza: {a.Raza} | Sonido: {a.HacerSonido()}");
+                        }
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Registro de persona.");
+                        Console.Write("Nombre: ");
+                        string nom = Console.ReadLine();
+                        Console.Write("DNI: ");
+                        string dni = Console.ReadLine();
+
+                        personasRegistradas.Add(new Persona(nom, dni));
+                        Console.WriteLine("Persona registrada.");
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Adopción.");
+                        Console.Write("Ingrese el DNI de la persona: ");
+                        string dniBuscado = Console.ReadLine();
+
+                        Persona personaEncontrada = null;
+                        foreach (var p in personasRegistradas)
+                        {
+                            if (p.Dni == dniBuscado) personaEncontrada = p;
+                        }
+
+                        if (personaEncontrada == null)
+                        {
+                            Console.WriteLine("La persona no existe.");
+                            break;
+                        }
+
+                        Console.Write("Ingrese el número del animal (Se ve cuando se muestran los animales disponibles): ");
+                        int indice = int.Parse(Console.ReadLine());
+
+                        if (indice < 0 || indice >= animalesDisponibles.Count)
+                        {
+                            Console.WriteLine("Ese animal no existe.");
+                            break;
+                        }
+
+                        Animal animalElegido = animalesDisponibles[indice];
+                        personaEncontrada.MascotasAdoptadas.Add(animalElegido);
+                        animalesDisponibles.RemoveAt(indice);
+
+                        Console.WriteLine($"{personaEncontrada.Nombre} adoptó a {animalElegido.Nombre}.");
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Adoptantes.");
+                        foreach (var p in personasRegistradas)
+                        {
+                            Console.WriteLine($"Adoptante: {p.Nombre} | DNI: {p.Dni}");
+                        }
+                        break;
+
+                    case "5":
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ [DarioMontero]-Act15/punto 3/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto_3
+{
+    class ReservaNatural {
+        private string nombreReserva;
+        private int[,] avistamientos;
+
+        public ReservaNatural() {
+            string linea;
+            avistamientos = new int[3, 3];
+            Console.WriteLine("escribe el nombre de la reserva: ");
+            nombreReserva = Console.ReadLine();
+            for (int i = 0; i < avistamientos.GetLength(0); i++) {
+                for (int f = 0; f < avistamientos.GetLength(1); f++) {
+                    Console.WriteLine("escribe por favor el numero de avistamientos que se hicieron en el sector de la fila numero " + i + " con la columna numero " + f);
+                    linea = Console.ReadLine();
+                    avistamientos[i, f] = int.Parse(linea);
+                }
+            }
+        }
+
+        public string devolverNombre() {
+            return nombreReserva;
+        }
+        public int[,] devolverAvistamientos() { 
+        return avistamientos;
+        }
+    }
+
+    class CentroEcologico
+    {
+        ReservaNatural[] reservas;
+
+        public CentroEcologico() {
+            reservas = new ReservaNatural[3];
+            for (int i = 0; i < reservas.Length; i++) {
+                reservas[i] = new ReservaNatural();
+            }
+        }
+
+        public void mostrarAvistamientos() {
+            int sumaTotal = 0;
+            for (int i = 0; i < reservas.Length; i++) {
+                sumaTotal = 0;
+                int[,]avistamientos = new int[3, 3];
+                avistamientos = reservas[i].devolverAvistamientos();
+                for (int f = 0; f < avistamientos.GetLength(0); f++) {
+                    for (int y = 0; y < avistamientos.GetLength(1); y++) {
+                        sumaTotal += avistamientos[f, y];
+                    }                
+                }
+                Console.WriteLine("la reserva " + reservas[i].devolverNombre() + " tuvo " + sumaTotal + " avistamientos totales");
+            }
+        }
+
+        public void masAvistamientos() {
+            int mayor = -1;
+            int filaMayor =-1;
+            int columnaMayor =-1;
+            string nombreReserva = "";
+            for (int i = 0; i < reservas.Length; i++)
+            {
+                int[,] avistamientos = new int[3, 3];
+                avistamientos = reservas[i].devolverAvistamientos();
+                
+                for (int f = 0; f < avistamientos.GetLength(0); f++)
+                {
+                    for (int y = 0; y < avistamientos.GetLength(1); y++)
+                    {
+                        if (avistamientos[f, y] > mayor) {
+                        mayor = avistamientos[f, y];
+                            filaMayor = f;
+                            columnaMayor = y;
+                            nombreReserva = reservas[i].devolverNombre();
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("la reserva " + nombreReserva + " tuvo la cantidad mas grande de todas las reservas con " + mayor + " y fue en la fila " + filaMayor + " y columna " + columnaMayor);
+        }
+
+        static void Main(string[] args)
+        {
+            CentroEcologico centro = new CentroEcologico();
+            centro.mostrarAvistamientos();
+            centro.masAvistamientos();
+            Console.ReadKey();
+        }
+    }
+}
