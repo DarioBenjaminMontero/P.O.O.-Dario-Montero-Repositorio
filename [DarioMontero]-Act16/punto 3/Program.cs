eb98185881f4a5b67d67343396d0be66da841005
DarioBenjaminMontero <dariomontero2008@gmail.com>
2026-06-05 11:54:19 -0300

    termino punto 1 y 2

---
+++ [DarioMontero]-Act16/punto 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto_3
+{
+
+    class Vehiculo {
+        string marca;
+        float velocidadMaxima;
+        public Vehiculo() {
+        
+        }
+
+
+    }
+
+    class Moto : Vehiculo 
+    {
+        private int cilindrada;
+    }
+
+    class Auto : Vehiculo 
+    {
+        private int numeroPuertas;
+        public Auto():base(numeroPuertas) {
+        
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
