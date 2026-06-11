091b84143c4de1d87310db99ed355f72aad60c53
DarioBenjaminMontero <dariomontero2008@gmail.com>
2026-06-11 09:30:17 -0300

    termino la act 16

--- [DarioMontero]-Act16/punto 3/Program.cs
+++ [DarioMontero]-Act16/punto 3/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +10,23 @@ namespace punto_3
 {
 
     class Vehiculo {
-        string marca;
-        float velocidadMaxima;
-        public Vehiculo() {
-        
+       private string marca;
+       private float velocidadMaxima;
+       private string linea;
+        public Vehiculo(string marcaPuesta, float velocidadPuesta){
+            marca = marcaPuesta;
+
+            velocidadMaxima = velocidadPuesta;
+        }
+        public string Marca
+        {
+            set { marca = value; }
+            get { return marca; }
+        }
+        public float VelocidadMaxima
+        {
+            set { velocidadMaxima = value; }
+            get { return velocidadMaxima; }
         }
 
 
@@ -20,18 +34,49 @@ namespace punto_3
 
     class Moto : Vehiculo 
     {
+
         private int cilindrada;
+        private string linea;
+        public Moto(string marca, float velocidadMaxima, int cilindrada) : base (marca, velocidadMaxima){
+            this.cilindrada = cilindrada;
+        }
+        public int Cilindrada {
+            set { cilindrada = value; } get { return cilindrada; }
+        }
+        public void imprimir() {
+            Console.WriteLine(base.Marca);
+            Console.WriteLine(base.VelocidadMaxima);
+            Console.WriteLine(this.Cilindrada);
+        }
     }
 
     class Auto : Vehiculo 
     {
+       private string linea;
         private int numeroPuertas;
-        public Auto():base(numeroPuertas) {
-        
+        public Auto(string marca, float velocidadMaxima, int numeroPuertas) : base (marca, velocidadMaxima) {
+            this.numeroPuertas = numeroPuertas;
+            
         }
-
+        public int NumeroPuertas
+        {
+            set { numeroPuertas = value; }
+            get { return numeroPuertas; }
+        }
+        public void imprimir()
+        {
+            Console.WriteLine(base.Marca);
+            Console.WriteLine(base.VelocidadMaxima);
+            Console.WriteLine(this.NumeroPuertas);
+        }
         static void Main(string[] args)
         {
+
+            Moto moto = new Moto("ford", 120, 12);
+            Auto auto = new Auto("toyota", 200, 12);
+            moto.imprimir();
+            auto.imprimir();
+            Console.ReadKey();
         }
     }
 }
---
+++ [DarioMontero]-Act16/punto 4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto_4
+{
+    class Animal {
+        private string especie;
+        public Animal() {
+            especie = "mamifero";
+
+            Console.WriteLine("la especie es " + especie + ". esta informacion viene de Animal");
+        }
+        public string Especie {
+            set { especie = value; } get { return especie; }
+        }
+    }
+    class Mamifero : Animal{
+       private string tipoAlimentacion;
+
+        public Mamifero() {
+            tipoAlimentacion = "comida xd";
+            Console.WriteLine("el tipo de alimentacion es " + tipoAlimentacion + ". esta informacion viene de Mamifero");
+        }
+
+        public string TipoAlimentacion
+        {
+            set { tipoAlimentacion = value; }
+            get { return tipoAlimentacion; }
+        }
+    }
+
+    internal class Perro : Mamifero
+    {
+        private string nombre;
+        public Perro() {
+            nombre = "chufo";
+            Console.WriteLine("el nombre es " + nombre + ". esta informacion viene de Perro");
+        }
+        public string Nombre
+        {
+            set { nombre = value; }
+            get { return nombre; }
+        }
+        static void Main(string[] args)
+        {
+            Animal perro = new Perro();
+            Console.ReadKey();
+        }
+    }
+}
