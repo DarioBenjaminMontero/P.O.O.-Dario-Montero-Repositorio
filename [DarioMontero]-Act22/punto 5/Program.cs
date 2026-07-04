4f15f719452150bfa279e5ba01fadc7b0438b21d
DarioBenjaminMontero <dariomontero2008@gmail.com>
2026-07-03 22:31:33 -0300

    termino la act 22

--- [DarioMontero]-Act22/punto 5/Program.cs
+++ [DarioMontero]-Act22/punto 5/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -20,9 +21,14 @@ namespace punto_5
             set { codigo = value; }
             get { return codigo; }
         }
-        private DateTime HoraFin
+        public DateTime HoraFin
         {
-            set { }
+            set { horaFin = value;  }
+            get { return horaFin; }
+        }
+        public DateTime HoraInicio {
+            set { horaInicio = value;  }
+            get { return horaInicio;  }
         }
 
         public Carrera() {
@@ -55,8 +61,102 @@ namespace punto_5
         private int numeroDorsal;
         private string nombre;
         TimeSpan tiempoTotal;
+        public string Nombre
+        {
+            set { nombre = value;  }
+            get { return nombre; }
+        }
+
+        public Corredor() {
+            Console.WriteLine("escribe el numero de dorsal");
+            numeroDorsal = int.Parse(Console.ReadLine());
+            Console.WriteLine("escribe el nombre");
+            nombre = Console.ReadLine();
+        }
+        public TimeSpan calcularDuracion(int minutos, int horas) {
+            int minutosTotales = minutos + horas * 60;
+            tiempoTotal = TimeSpan.FromMinutes(minutosTotales);
+            return tiempoTotal;
+        }
+        public TimeSpan calcularDuracion(int minutos)
+        {
+       
+            tiempoTotal = TimeSpan.FromMinutes(minutos);
+            return tiempoTotal;
+        }
+        public TimeSpan TiempoTotal
+        {
+            get { return tiempoTotal; }
+        }
+
         static void Main(string[] args)
         {
+            Corredor[] corredores = new Corredor[8];
+            for (int i = 0; i < 8; i++)
+            {
+                corredores[i] = new Corredor();
+
+            }
+            corredores[0].calcularDuracion(120);
+                corredores[1].calcularDuracion(100);
+            corredores[2].calcularDuracion(90);
+            corredores[3].calcularDuracion(150);
+            corredores[4].calcularDuracion(10,1);
+                corredores[5].calcularDuracion(15, 2);
+            corredores[6].calcularDuracion(10, 2);
+            corredores[7].calcularDuracion(20, 1);
+            Carrera[] carreras = new Carrera[4];
+            carreras[0] = new Carrera();
+            carreras[1] = new Carrera();
+            carreras[2] = new Carrera(3, 14,16);
+            carreras[3] = new Carrera(4,15,16);
+            carreras[0].agregarCorredor(corredores[0]);
+            carreras[0].agregarCorredor(corredores[1]);
+            carreras[1].agregarCorredor(corredores[2]);
+            carreras[1].agregarCorredor(corredores[3]);
+            carreras[2].agregarCorredor(corredores[4]);
+            carreras[2].agregarCorredor(corredores[5]);
+            carreras[3].agregarCorredor(corredores[6]);
+            carreras[3].agregarCorredor(corredores[7]);
+            TimeSpan menorTiempo = corredores[0].TiempoTotal;
+            for (int i = 0; i < corredores.Length; i++) {
+                if (menorTiempo > corredores[i].TiempoTotal) {
+                    menorTiempo = corredores[i].TiempoTotal;
+                }
+            }
+            Console.Clear();
+            int posiciony = 10;
+            for (int i = 0; i < corredores.Length; i++)
+            {
+                if (menorTiempo == corredores[i].TiempoTotal)
+                {
+                    
+                    Console.SetCursorPosition(0, posiciony);
+                    Console.WriteLine("el corredor de nombre " + corredores[i].Nombre + "hizo el menor tiempo con " + menorTiempo);
+                    posiciony += 10;
+                }
+            }
+            DateTime masTemprano = carreras[0].HoraInicio;
+            for(int i = 0; i<carreras.Length; i++){
+                if (masTemprano > carreras[i].HoraInicio) {
+                    masTemprano = carreras[i].HoraInicio;
+                }
+            }
+            posiciony = 10;
+
+            for (int i = 0; i < carreras.Length; i++)
+            {
+                if (masTemprano == carreras[i].HoraInicio)
+                {
+                    
+                    Console.SetCursorPosition(40, posiciony);
+                    Console.WriteLine("codiga de la mas temprana:" + carreras[i].Codigo);
+                    posiciony += 10;
+                }
+            }
+
+
+
         }
     }
 }
