819de79118f511a927647ab937567b641346c2da
DarioBenjaminMontero <dariomontero2008@gmail.com>
2026-08-06 09:59:14 -0300

    cambios

---
+++ [DarioMontero]-Act23/punto 1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto_1
+{
+
+    class Documento
+    {
+        private string nombreArchivo;
+        private int cantidadDePaginas;
+        public string NombreArchivo
+        {
+            set { nombreArchivo = value; }
+            get { return nombreArchivo; }
+
+        }
+        public int CantidadDePaginas
+        {
+            set { cantidadDePaginas = value; }
+            get { return cantidadDePaginas; }
+        }
+        public Documento(string nom, int pag)
+        {
+            nombreArchivo = nom;
+            cantidadDePaginas = pag;
+        }
+
+    }
+    class ServidorImpresion
+    {
+        List<Documento>Documentos = new List<Documento>();
+        public void AgregarDocumento() {
+            string linea, nombre;
+            int cantPaginas;
+            Console.WriteLine("Por favor, ingrese el nombre del archivo");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Por favor, ingrese el numero de paginas que tiene el archivo");
+            linea = Console.ReadLine();
+            cantPaginas = int.Parse(linea);
+            Documento documento = new Documento(nombre, cantPaginas);
+            Documentos.Add(documento);
+            Console.WriteLine("Agregado exitosamente el archivo " + nombre + " que tiene " + cantPaginas);
+        }
+        public void ImprimirSiguiente() {
+            if (Documentos.Count == 0)
+            {
+                Console.WriteLine("no hay documentos pendientes a imprimir");
+            }
+            else {
+                Console.WriteLine("El documento que se imprimió fue: " + Documentos[0].NombreArchivo);
+                Console.WriteLine("El documento cuenta con " + Documentos[0].CantidadDePaginas + " paginas");
+                Documentos.RemoveAt(0);
+            }
+        }
+        public void MostrarColaPendiente() {
+            if (Documentos.Count == 0)
+            {
+                Console.WriteLine("no hay documentos pendientes");
+            }
+            else {
+                int cantidadDePaginas = 0;
+                foreach (Documento d in Documentos)
+                {
+                    Console.WriteLine("Documento: " + d.NombreArchivo + ". Cantidad de Paginas: " + d.CantidadDePaginas);
+                    cantidadDePaginas = cantidadDePaginas + d.CantidadDePaginas;
+                }
+                Console.WriteLine("la cantidad de Paginas en total son: " + cantidadDePaginas);
+                Console.WriteLine("la cantidad de Documentos pendientes son: " + Documentos.Count);
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            ServidorImpresion servidor = new ServidorImpresion();
+            for (int i = 0; i < 1; i++) {
+                Console.WriteLine("1 para agregar, 2 para imprimir el siguiente documento, 3 para mostrar los pendientes, 4 para acabar el programa.");
+                int numero = int.Parse(Console.ReadLine());
+                if (numero < 1 || numero > 4)
+                {
+                    Console.WriteLine("Numero invalido, intente otra vez");
+                    i--;
+                }
+                else if (numero == 1) {
+                    servidor.AgregarDocumento();
+                    i--;
+                }
+                else if (numero == 2)
+                {
+                    servidor.ImprimirSiguiente();
+                    i--;
+                }
+                else if (numero == 3)
+                {
+                    servidor.MostrarColaPendiente();
+                    i--;
+                }
+                else if (numero == 4)
+                {}
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ [DarioMontero]-Act23/punto 2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto_2
+{
+    class Dron {
+        private string codigo;
+        private int bateria;
+
+        public string Codigo
+        {
+            set { codigo = value; }
+            get { return codigo; }
+        }
+        public int Bateria
+        {
+            set { bateria = value; }
+            get { return bateria; }
+        }
+        public Dron(string cod, int bat) {
+            bateria = bat;
+            codigo = cod;
+        }
+    }
+    class CentroControl
+    {
+        List<Dron> Drones = new List<Dron>();
+
+        public CentroControl() {
+            string nombre;
+            int bateria =0 ;
+            for (int i = 0; i < 4; i++) {
+                Console.WriteLine("Ingrese el nombre del Dron");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese la bateria que tiene el dron, tiene que ser mayor a 0 y menor a 100");
+                for (int f = 0; f < 1; f++)
+                {
+                    bateria = int.Parse(Console.ReadLine());
+                    if (bateria < 0 || bateria > 100)
+                    {
+                        Console.WriteLine("Bateria invalida, intente de nuevo");
+                        f--;
+                    }       
+                }
+                Dron dron = new Dron(nombre, bateria);
+                Drones.Add(dron);
+            }
+        }
+        public void ListarFlota() {
+            foreach (Dron d in Drones) {
+                Console.WriteLine("Codigo del dron: " + d.Codigo + ". Bateria del dron: " + d.Bateria);
+            }
+        }
+        public void RemoverDronesBajos() {
+            for (int i = Drones.Count-1; i > 0; i--) {
+                if (Drones[i].Bateria < 15) {
+                    Console.WriteLine("el dron con el codigo " + Drones[i].Codigo + " tiene una bateria de " + Drones[i].Bateria + " y necesita mantenimiento urgentemente");
+                Drones.RemoveAt(i);
+                }
+            }
+        }
+        public void MostrarDronesRestantes() {
+            foreach (Dron d in Drones) {
+                Console.WriteLine("los drones que quedan activos son");
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
